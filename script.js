2b51e394798515e858eeeb11a60d8db3231b9419
Pawski-studio <pawski.studio@gmail.com>
2025-02-03 20:04:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ menuOpenButton.addEventListener("click", () => {
 
     menuCloseButton.addEventListener("click", () => menuOpenButton.click
         ());
+
+   //    function playVid(){ 
+     //       $('hero-video-wrapper').get(0).play();
+      //     setTimeout(playVid, 3000); 
+
+      let section = document.querySelectorAll('section');
+      let navLink = document.querySelectorAll('header nav a');
+
+      window.onscroll = () => {
+        sections.forEach(sec => {
+          let top = window.scrollY;
+          let offset = sec.offsetTop - 150;
+          let height = sec.offsetHeight;
+          let id = sec.getAttribute('id');
+
+          if (top >= offset && top < offset + height){
+            navLink.forEach(links => {
+              links.classList.remove('active');
+              document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+            });
+          };
+        });
+      };
